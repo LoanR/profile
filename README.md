2e55c816cc11590d670431294b1d469c0d7cc9c0
Loan Robert <loan.rbrt@gmail.com>
2016-09-08 17:45:33 +0200

    updated profile summer 2016

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 http://LoanR.github.io/profile
+
+Page statique (Github Pages oblige) de mon profil.
+
+Html5, CSS3, Jquery, Bootstrap, Mediaqueries...
--- js/events.js
+++ js/events.js
@@ -9,18 +9,3 @@ $(document).on('click', "a[href='#top']", function(event){
   event.preventDefault();
   $("html, body").animate({ scrollTop: 0 }, 600);
 });
-
-// $(function() {
-//   $('a[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-//       var target = $(this.hash);
-//       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-//       if (target.length) {
-//         $('html, body').animate({
-//           scrollTop: target.offset().top
-//         }, 1000);
-//         return false;
-//       }
-//     }
-//   });
-// });
